089dac2f929f34eb34773c9a6d81447a7b9a1978
Nithi1991 <n.bharathan43@gmail.com>
2023-06-28 11:28:50 +0530

    tree

---
+++ dataStructures/tree/constructor.js
@@ -0,0 +1,15 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST{
+  constructor(){
+    this.root = null
+  }
+}
+let mytree = new BST()
+mytree
